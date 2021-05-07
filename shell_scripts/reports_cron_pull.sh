9570e44fd0b4883f6f89e477a68dce425209bcd8
matildastevenson <matildastevenson2812@gmail.com>
2021-05-07 15:49:04 +1000

    Add another app to pull

--- shell_scripts/reports_cron_pull.sh
+++ shell_scripts/reports_cron_pull.sh
@@ -2,3 +2,6 @@
 cd /data/ala-reporting
 git pull
 cp /data/ala-reporting/reports/* /srv/shiny-server/apps/reports/
+
+# copy data trends app: note- do a similar thing for other apps
+cp -r /data/ala-reporting/apps/ala_data /srv/shiny-server/apps
