3f3ff0ccb74acf51f25ce85dbf56461955d1e209
matildastevenson <matildastevenson2812@gmail.com>
2021-05-07 14:36:40 +1000

    Update website build script

--- shell_scripts/website_build.sh
+++ shell_scripts/website_build.sh
@@ -4,4 +4,6 @@ cd /data/science
 git pull
 cd analytics_site
 Rscript build_site.R
+rm -r /srv/shiny-server/site_libs/
 cp docs/index.html /srv/shiny-server/
+cp -r /data/science/analytics_site/docs/site_libs /srv/shiny-server/
