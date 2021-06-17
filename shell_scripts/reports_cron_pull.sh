cc46c80bf572c9113229c42581243e9f99b61c1c
matildastevenson <matildastevenson2812@gmail.com>
2021-06-17 14:47:22 +1000

    Update shell script

--- shell_scripts/reports_cron_pull.sh
+++ shell_scripts/reports_cron_pull.sh
@@ -4,4 +4,4 @@ git pull
 cp /data/ala-reporting/reports/* /srv/shiny-server/apps/reports/
 
 # copy data trends app: note- do a similar thing for other apps
-cp -r /data/ala-reporting/apps/ala_data /srv/shiny-server/apps
+cp -r /data/ala-reporting/apps/data_trends /srv/shiny-server/apps/
